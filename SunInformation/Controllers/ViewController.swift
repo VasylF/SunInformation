b5a30bda633dcdc8c117414e45cbdc805818c94e
VasylF <v.fuchenko@gmail.com>
2018-06-08 13:08:37 +0300

    Automatic location detection

--- SunInformation/Controllers/ViewController.swift
+++ SunInformation/Controllers/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController, UISearchBarDelegate {
+class ViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var viewImage: UIImageView!
     @IBOutlet weak var sunsetLabel: UILabel!
@@ -18,12 +18,34 @@ class ViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var cityLabel: UILabel!
     
     var sunInformation: Information!
-
+    let locationManager = CLLocationManager()
+    var latitude: Double!
+    var longitude: Double!
+    var location: CLLocation! {
+        didSet {
+            latitude = location.coordinate.latitude
+            longitude = location.coordinate.longitude
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
-        updateSunInformationForLocation(location: "Lviv")
+        searchBar.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        checkCoreLocationPermission()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+    }
+    
+    func checkCoreLocationPermission() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        } else if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        } else if CLLocationManager.authorizationStatus() == .restricted {
+            print("unauthorized to use location service")
+        }
     }
 
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
@@ -32,6 +54,41 @@ class ViewController: UIViewController, UISearchBarDelegate {
             updateSunInformationForLocation(location: locationString)
             cityLabel.text = locationString
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.last
+        locationManager.stopUpdatingLocation()
+        if latitude != nil, longitude != nil {
+            getSunInformationOnlyWithCoordinate(latitude: latitude, longitude: longitude)
+        }
+        getCityName()
+    }
+    
+    func getCityName() {
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            if error == nil {
+                var placemark: CLPlacemark!
+                placemark = placemarks![0]
+                if let city = placemark?.locality {
+                    self.cityLabel.text = String(city)
+                } else {
+                    self.cityLabel.text = "City didn't found"
+                }
+            }
+        }
+    }
+    
+    func getSunInformationOnlyWithCoordinate(latitude: Double, longitude: Double) {
+        SunService.getSunInformation(latitude: latitude, longitude: longitude, completion: { (result) in
+            if let sunInformationData = result {
+                self.sunInformation = sunInformationData
+                DispatchQueue.main.async {
+                    self.updateLabel(info: self.sunInformation)
+                }
+            }
+        })
     }
     
     func updateLabel(info: Information) {
